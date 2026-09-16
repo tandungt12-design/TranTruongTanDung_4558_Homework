6dd67d3b59e0c7971c3dd708455fef15ba0da288
Tran Dung <tandungt12@gmail.com>
2026-09-16 23:21:42 +0700

    #1-Add Exercise 3,4

---
+++ HomeworkAbstract/Exercise4/Processor.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package HomeworkAbstract.Exercise4;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Processor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        Circle a = new Circle();
+        a.draw();
+        a.fillColor();
+    }
+}
